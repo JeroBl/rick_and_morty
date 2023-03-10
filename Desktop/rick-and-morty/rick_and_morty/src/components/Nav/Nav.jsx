03bd8e6c5c2e8377c45f2ad2647b470788efe723
JeroBl <jeroblanco44@gmail.com>
2023-03-09 23:12:07 -0300

    HW react estados

---
+++ Desktop/rick-and-morty/rick_and_morty/src/components/Nav/Nav.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import SearchBar from "../SearchBar/SearchBar";
+
+
+class Nav extends React.Component {
+    // constructor(props) {
+    //     super(props);
+    // }
+
+    render(){
+        return (
+            <div>
+                <SearchBar onSearch = {this.props.onSearch} />
+            </div>
+        )
+    }
+}
+
+export default Nav;
